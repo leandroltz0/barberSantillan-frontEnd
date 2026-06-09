04d0e62b7c637e44976c175ada5642aabcd91428
lean <leanmaci810@gmail.com>
2026-06-09 01:17:52 -0300

    feat: add Vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   server: { port: 4321 },
   vite: {
     server: {
